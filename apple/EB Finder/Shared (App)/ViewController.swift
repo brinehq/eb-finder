67edb69cf32435d03dd2872cfe47b02f806a4977
Ronald Pompa <ronald@brine.co>
2026-05-13 17:44:01 +0200

    fix: fix bundle ids

--- apple/EB Finder/Shared (App)/ViewController.swift
+++ apple/EB Finder/Shared (App)/ViewController.swift
@@ -16,7 +16,7 @@ import SafariServices
 typealias PlatformViewController = NSViewController
 #endif
 
-let extensionBundleIdentifier = "com.brine.ebfinder.Extension"
+let extensionBundleIdentifier = "com.brine.ebfinder.extension"
 
 class ViewController: PlatformViewController, WKNavigationDelegate, WKScriptMessageHandler {
 
