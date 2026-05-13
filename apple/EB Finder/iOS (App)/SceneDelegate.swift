c43e395787ed8bc06e34939d1c388a60c030be7e
Ronald Pompa <ronald@brine.co>
2026-05-13 14:54:40 +0200

    feat: SAS EuroBonus Finder Safari extension

---
+++ apple/EB Finder/iOS (App)/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  iOS (App)
+//
+//  Created by Ronald Pompa on 2026-05-13.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+}
